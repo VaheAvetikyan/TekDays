8679349370d89ea68eabc0b74dcfd27eeb454a94
VaheAvetikyan <vahe.avetikyan@gmail.com>
2021-03-23 18:18:01 +0400

    added database configurations in local configs, datasource reverted to H2 db, BuildConfig.groovy dependency with mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,6 @@
 dataSource {
     pooled = true
     jmxExport = true
-//    driverClassName = "com.mysql.jdbc.Driver"
-//    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
@@ -22,9 +20,6 @@ environments {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-//            url = "jdbc:mysql://localhost:3306/tekdays"
-//            username = "root"
-//            password = ""
         }
     }
     test {
