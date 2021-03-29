b38f2ff48bc063e42f559010537c2837fbc56882
VaheAvetikyan <vahe.avetikyan@gmail.com>
2021-03-29 17:34:10 +0400

    security layer added, but not yet working

--- grails-app/controllers/com/tekdays/TekUserController.groovy
+++ grails-app/controllers/com/tekdays/TekUserController.groovy
@@ -1,7 +1,6 @@
 package com.tekdays
 
 
-
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
@@ -12,11 +11,38 @@ class TekUserController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond TekUser.list(params), model:[tekUserInstanceCount: TekUser.count()]
+        respond TekUser.list(params), model: [tekUserInstanceCount: TekUser.count()]
     }
 
     def show(TekUser tekUserInstance) {
         respond tekUserInstance
+    }
+
+    def login() {
+        if (params.cName) {
+            return [cName: params.cName, aName: params.aName]
+        }
+    }
+
+    def logout() {
+        session.user = null
+        redirect(uri:'/')
+    }
+
+    def validate() {
+        def user = TekUser.findByUserName(params.username)
+        if (user && user.password == params.password) {
+            session.user = user
+
+            if (params.cName) {
+                redirect controller: params.cName, action: params.aName
+            } else {
+                redirect controller: 'tekEvent', action: 'index'
+            }
+        } else {
+            flash.message = "Invalid username and password."
+            render view: 'login'
+        }
     }
 
     def create() {
@@ -31,11 +57,11 @@ class TekUserController {
         }
 
         if (tekUserInstance.hasErrors()) {
-            respond tekUserInstance.errors, view:'create'
+            respond tekUserInstance.errors, view: 'create'
             return
         }
 
-        tekUserInstance.save flush:true
+        tekUserInstance.save flush: true
 
         request.withFormat {
             form multipartForm {
@@ -58,18 +84,18 @@ class TekUserController {
         }
 
         if (tekUserInstance.hasErrors()) {
-            respond tekUserInstance.errors, view:'edit'
+            respond tekUserInstance.errors, view: 'edit'
             return
         }
 
-        tekUserInstance.save flush:true
+        tekUserInstance.save flush: true
 
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.updated.message', args: [message(code: 'TekUser.label', default: 'TekUser'), tekUserInstance.id])
                 redirect tekUserInstance
             }
-            '*'{ respond tekUserInstance, [status: OK] }
+            '*' { respond tekUserInstance, [status: OK] }
         }
     }
 
@@ -81,14 +107,14 @@ class TekUserController {
             return
         }
 
-        tekUserInstance.delete flush:true
+        tekUserInstance.delete flush: true
 
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.deleted.message', args: [message(code: 'TekUser.label', default: 'TekUser'), tekUserInstance.id])
-                redirect action:"index", method:"GET"
+                redirect action: "index", method: "GET"
             }
-            '*'{ render status: NO_CONTENT }
+            '*' { render status: NO_CONTENT }
         }
     }
 
@@ -98,7 +124,7 @@ class TekUserController {
                 flash.message = message(code: 'default.not.found.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])
                 redirect action: "index", method: "GET"
             }
-            '*'{ render status: NOT_FOUND }
+            '*' { render status: NOT_FOUND }
         }
     }
 }
