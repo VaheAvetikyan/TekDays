dc5981244833f53202dfd5f1793eca8130949a87
VaheAvetikyan <vahe.avetikyan@gmail.com>
2021-11-16 00:02:21 +0400

    Set registerNewUser action to POST method

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,8 +5,7 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require jquery
-//= require_tree .
+//= require jquery-3.3.1.min
 //= require jquery-ui
 //= require jquery.dataTables
 //= require_self
